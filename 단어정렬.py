11abbfc20bfb657777bfda9d4cc6201f67ddd0bd
Seman_Kim <sdoramkim@gmail.com>
2023-05-05 20:48:50 +0900

    단어 정렬 word를 key로 사용하는 풀이 추가
    TIL 작성 중 dict의 key를 word로 받으면 중복 제거 과정도 필요없고 충분히
    가능할 것 같아서 처음에 만들었던 dict형태를 다시 시도했다. 시간과 메모리
    모두 효율적이지 못한 코드지만 dict의 사용방법을 하나 추가했다는 것에
    매우 만족한다.

--- 단어정렬.py
+++ 단어정렬.py
@@ -24,3 +24,32 @@ for num in sorted(word_dict):
     # 밸류를 set으로 중복 제거, sorted로 사전순 정렬
     for word in sorted(set(word_dict[num])):
         print(word)
+
+word_dict = {
+    'but': 3,
+    'i': 1,
+    'wont': 4,
+    'hesitate': 8,
+    'no': 2,
+    'more': 4,
+    'it': 2,
+    'cannot': 6,
+    'wait': 4,
+    'im': 2,
+    'yours': 5
+}
+
+word_dict = {}
+# 테스트 케이스만큼 입력 받기
+for _ in range(int(input())):
+    word = input()
+    # key가 문자, value가 단어의 길이로 딕셔너리 넣기
+    # 중복인 경우 겹치므로 중복 제거 필요 X
+    if word not in word_dict:
+        word_dict[word] = [len(word)]
+# word를 사전순으로 먼저 정렬
+word_dict = sorted(word_dict.items())
+# 길이를 기준으로 재정렬
+word_dict = sorted(word_dict, key=lambda value: value[1])
+for word in word_dict:
+    print(word[0])
