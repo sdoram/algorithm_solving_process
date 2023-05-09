9644050a3e08d508d50aaea88295cab948618eaa
Seman_Kim <sdoramkim@gmail.com>
2023-05-09 23:44:18 +0900

    보약 흡수
    팀 프로젝트를 하면서 시간관리 실패
    가장 쉬운 브론즈5만 뽑아서 5문제 억지로 채움

---
+++ 23-05-09/연세대학교.py
@@ -0,0 +1,4 @@
+# https://www.acmicpc.net/problem/15680
+
+N = int(input())
+print('Leading the Way to the Future' if N else 'YONSEI')
---
+++ 23-05-09/홍익대학교.py
@@ -0,0 +1,5 @@
+# https://www.acmicpc.net/problem/16394
+
+A = 1946
+B = int(input())
+print(B - A)
