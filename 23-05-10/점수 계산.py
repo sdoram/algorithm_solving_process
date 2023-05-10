51a006583572dce8303de387af8b8e4bf3edd4ef
Seman_Kim <sdoramkim@gmail.com>
2023-05-10 13:30:14 +0900

    input()을 가지고 있는 list comprehension 만들기
    점수 계산.py : commit 메시지를 작성하는 과정에서 생긴 의문을
    시험해봤는데 list comprehension으로 잘 작성된다.

--- 23-05-10/점수 계산.py
+++ 23-05-10/점수 계산.py
@@ -4,9 +4,7 @@
 
 # 출력 총점
 # 포함되는 문제 번호 
-score_list = []
-for num in range(1,9):
-    score_list.append([int(input()),num])    
+score_list = [[int(input()),num] for num in range(1,9)]
 five_scores = sorted(score_list, reverse=True)[:5]
 print(sum([score[0] for score in five_scores]))
 print(*sorted([score[1] for score in five_scores]))
