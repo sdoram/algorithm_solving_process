834c67f2fc1252a08943a0ec5e27a039589968b4
Seman_Kim <sdoramkim@gmail.com>
2023-06-04 22:47:28 +0900

    sorted key lambda 활용
    key=lambda의 사용이 익숙하지 않아서 2번에 나눠서 사용했는데
    리팩토링 과정에서 1개의 sorted에서 모든 정렬을 할 수 있었고
    시간 복잡도의 개선도 있었다

---
+++ 23-06-04/키보드 이벤트.py
@@ -0,0 +1,37 @@
+# https://www.acmicpc.net/problem/17254
+N, M = 3, 5
+input_list = [
+    ["1", "0", "A"],
+    ["2", "1", "P"],
+    ["1", "2", "L"],
+    ["2", "4", "E"],
+    ["3", "0", "P"],
+]
+input_list2 = [
+    ["1", "6", "B"],
+    ["1", "1", "A"],
+    ["1", "0", "L"],
+    ["1", "3", "D"],
+    ["2", "8", "G"],
+    ["2", "6", "U"],
+    ["2", "3", "Y"],
+]
+sorted_list1 = sorted(input_list, key=lambda x: [int(x[1]), int(x[0])])
+# sorted_list1 = sorted(input_list, key=lambda x: x[0])
+print(sorted_list1)
+# sorted_list2 = sorted(input_list, key=lambda x: x[1])
+# print(sorted_list2)
+
+sorted_list1 = sorted(input_list2, key=lambda x: [int(x[1]), int(x[0])])
+# sorted_list1 = sorted(input_list2, key=lambda x: int(x[0]))
+print(sorted_list1)
+# sorted_list2 = sorted(input_list2, key=lambda x: int(x[1]))
+# print(sorted_list2)
+
+N, M = map(int, input().split())
+input_list = []
+for _ in range(M):
+    input_list.append(input().split())
+
+input_list = sorted(input_list, key=lambda x: [int(x[1]), int(x[0])])
+print("".join([x[2] for x in input_list]))
